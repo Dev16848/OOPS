e78e7479a58cd67794038de8b35f14b1dec35370
Dev Dhameliya <devdhameliya137@gmail.com>
2026-01-08 08:59:45 +0530

    Add files via upload

---
+++ Cramers_Rule.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Cramers_Rule{
+	public static void main(String args[]){
+		int a,b,c,d,e,f,D,Dx,Dy,x,y;
+
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("Enter the value of coefficient a:");
+		a=sc.nextInt();
+
+		System.out.println("Enter the value of coefficient b:");
+		b=sc.nextInt();
+		
+		System.out.println("Enter the value of coefficient c:");
+		c=sc.nextInt();
+
+		System.out.println("Enter the value of coefficient d:");
+		d=sc.nextInt();
+
+		System.out.println("Enter the value of coefficient e:");
+		e=sc.nextInt();
+
+		System.out.println("Enter the value of coefficient f:");
+		f=sc.nextInt();
+
+		
+		D=a*d-b*c;
+		Dx=e*d-b*f;
+		Dy=a*f-c*e;
+		x=Dx/D;
+		y=Dy/D;
+
+		
+		System.out.println("Value of x:"+x);
+		System.out.println("Value of y:"+y);
+
+		sc.close();
+		
+	}
+}
