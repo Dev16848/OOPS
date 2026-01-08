54a4784e8f3e1e4b0495004a133f1881f9e254a9
Dev Dhameliya <devdhameliya137@gmail.com>
2026-01-08 10:39:43 +0530

    Add files via upload
    To check whether the given character is a vowel or a consonant.

---
+++ Vowel.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Vowel{
+	public static void main(String args[]){
+			
+		char v;
+		
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Enter a character:");
+		v=sc.next().charAt(0);
+
+		if( v == 'a' || v == 'A' || v == 'e' || v == 'E' || v == 'i' || v == 'I' || v == 'o' || v == 'O' || v == 'u' || v == 'U')
+			System.out.println(v+" is a Vowel");
+		else
+			System.out.println(v+" is a Consonant ");
+
+
+		sc.close();
+	}
+}
